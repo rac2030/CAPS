b01c2ee9ed3af161481121acd223d60d578a3057
rac2030 <m@rac.su>
2015-02-24 23:36:28 +0100

    Adding simple unit test for StreamCopy, probably could need more but at least it tests copy of a big ASCII String

--- src/test/java/ch/racic/caps/utils/StreamCopyThreadTest.java
+++ src/test/java/ch/racic/caps/utils/StreamCopyThreadTest.java
@@ -1,14 +1,20 @@
 package ch.racic.caps.utils;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * StreamCopyThread Tester.
  *
- * @author <Authors name>
+ * @author Michel Racic
  * @version 1.0
  * @since <pre>Feb 22, 2015</pre>
  */
@@ -23,9 +29,17 @@ public class StreamCopyThreadTest {
     }
 
     @Test
-    public void testStreamCopy() throws Exception {
-        //TODO: Test goes here...
-        Assert.fail("Not implemented");
+    public void testSimpleString() throws Exception {
+        String testString = RandomStringUtils.randomAscii(5000000);
+        ByteArrayInputStream in = new ByteArrayInputStream(testString.getBytes(StandardCharsets.UTF_8));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        StreamCopyThread sct = new StreamCopyThread(in, out, 40960, false);
+        sct.start();
+        sct.join();
+        in.close();
+        out.close();
+        String transportedResult = out.toString(StandardCharsets.UTF_8.name());
+        assertThat("Output String is the same as InputString", testString.equals(transportedResult));
     }
 
 } 
